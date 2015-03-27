a85b2da74f804af7329e3dcd2b81d9f0af1f66f5
Jaap van Otterdijk <jaap@youngguns.nl>
2015-03-27 11:46:04 +0100

    add licence

--- tests/Paginator/Adapter/EntityServiceTest.php
+++ tests/Paginator/Adapter/EntityServiceTest.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Polder Knowledge / Entity Service (http://polderknowledge.nl)
+ *
+ * @link http://developers.polderknowledge.nl/gitlab/polderknowledge/entityservice for the canonical source repository
+ * @copyright Copyright (c) 2015-2015 Polder Knowledge (http://www.polderknowledge.nl)
+ * @license http://polderknowledge.nl/license/proprietary proprietary
+ */
 
 namespace PolderKnowledge\EntityServiceTest\Paginator\Adapter;
 
